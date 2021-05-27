b21b8ed9c021428ad73ef20e8aba83d299a1ef6e
Juan Olmedilla <juanjo@olmedilla.com>
2021-05-27 12:25:30 +0200

    Refactor HierarchyServiceTest to use mocks only as an end to earlier refactorings where I extracted tests from HierarchyServiceTest into tests to new components.

--- src/test/java/com/personia/hr/facade/HierarchyServiceTest.java
+++ src/test/java/com/personia/hr/facade/HierarchyServiceTest.java
@@ -1,52 +1,59 @@
 package com.personia.hr.facade;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.personia.hr.algorithm.FloydsTraverseAlgorithm;
-import com.personia.hr.algorithm.GraphCycleDetector;
 import com.personia.hr.exception.EmployeeHasTwoSupervisorsException;
 import com.personia.hr.exception.LoopInEmployeeHierarchyException;
 import com.personia.hr.exception.MultipleRootHierarchyException;
 import com.personia.hr.model.EmployeeDto;
-import com.personia.hr.model.HierarchySimpleImpl;
+import com.personia.hr.model.Hierarchy;
 import com.personia.hr.parser.JsonEmployeeToSupervisorParser;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashMap;
-import java.util.stream.Stream;
+import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
-public class HierarchyServiceTest {
+@ExtendWith(MockitoExtension.class)
+class HierarchyServiceTest {
 
-    private final HierarchyService hierarchyService = new HierarchyService(
-            new JsonEmployeeToSupervisorParser(
-                    new ObjectMapper(),
-                    new GraphCycleDetector(new FloydsTraverseAlgorithm())),
-            new HierarchySimpleImpl(new HashMap<>()));
+    @InjectMocks
+    private HierarchyService hierarchyService;
 
-    static Stream<Arguments> provideCorrectSamplesWithTheirRoots() {
-        return Stream.of(
-                Arguments.of("{\"Pete\":\"Nick\"}", "Nick"),
-                Arguments.of("{\"Pete\":\"Nick\",\"Barbara\":\"Nick\",\"Nick\":\"Sophie\"}", "Sophie"),
-                Arguments.of("{\"Pe,te\":\"Nick\"}", "Nick")
-        );
+    @Mock
+    private JsonEmployeeToSupervisorParser parser;
+
+    @Mock
+    private Hierarchy hierarchy;
+
+    @Test
+     void shouldReturnEmptyHierarchyWhenReceivesNoSupervisors() throws EmployeeHasTwoSupervisorsException, MultipleRootHierarchyException, LoopInEmployeeHierarchyException, JsonProcessingException {
+        when(parser.parse(any(String.class))).thenReturn(new HashMap<>());
+        when(hierarchy.getRoot()).thenReturn(EmployeeDto.builder().build());
+        hierarchyService.update("{}");
+        verify(hierarchy).getRoot();
+        verifyNoMoreInteractions(hierarchy);
     }
 
     @Test
-    public void shouldReturnEmptyHierarchyWhenReceivesNoSupervisors() throws EmployeeHasTwoSupervisorsException, MultipleRootHierarchyException, LoopInEmployeeHierarchyException, JsonProcessingException {
-        EmployeeDto hierarchy = hierarchyService.update("{}");
-        assertThat(hierarchy.getName()).isNull();
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideCorrectSamplesWithTheirRoots")
-    public void shouldReturnBasicHierarchyWhenReceivesOneSupervisor(String relationships, String root) throws EmployeeHasTwoSupervisorsException, MultipleRootHierarchyException, LoopInEmployeeHierarchyException, JsonProcessingException {
-        EmployeeDto hierarchy = hierarchyService.update(relationships);
-        assertThat(hierarchy.getName()).isEqualTo(root);
+     void shouldReturnBasicHierarchyWhenReceivesOneSupervisor() throws EmployeeHasTwoSupervisorsException, MultipleRootHierarchyException, LoopInEmployeeHierarchyException, JsonProcessingException {
+        when(parser.parse(any(String.class))).thenReturn(Map.of("Pete","Barbara","Barbara","Nick"));
+        when(hierarchy.getRoot()).thenReturn(EmployeeDto.builder().build());
+        hierarchyService.update("{}");
+        verify(parser).parse(any(String.class));
+        verify(hierarchy).getRoot();
+        verify(hierarchy).add("Pete","Barbara");
+        verify(hierarchy).add("Barbara","Nick");
+        verifyNoMoreInteractions(hierarchy);
     }
 
 }
