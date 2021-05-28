4516ff6f7f83797501b7945282a7e9fe06abe3ba
Juan Olmedilla <juanjo@olmedilla.com>
2021-05-28 10:26:21 +0200

    Documenting Cycle detection algorithm in javadoc

--- src/main/java/com/personia/hr/algorithm/FloydsTraverseAlgorithm.java
+++ src/main/java/com/personia/hr/algorithm/FloydsTraverseAlgorithm.java
@@ -1,12 +1,24 @@
 package com.personia.hr.algorithm;
 
+import com.personia.hr.exception.LoopInEmployeeHierarchyException;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class FloydsTraverseAlgorithm {
 
+    /**
+     * Implementation of the first bit, cycle detection, of Floyd's tortoise and hare algorithm
+     * (see See <a href="https://en.wikipedia.org/wiki/Cycle_detection">http://https://en.wikipedia.org/wiki/Cycle_detection</a>)
+     * This part of the algorithm is O(lambda) being lambda the length of the cycle or the way to the top of the
+     * hierarchy if there is no loop starting at this node
+     *
+     * @param map the function with that maps employee->supervisor
+     * @param x_0 the employee for which we want to traverse the map up to the root
+     * @return true if there is a loop starting at this employee
+     */
     public boolean traverse(final Map<String,String> map, String x_0) {
         String tortoise = map.get(x_0);
         String hare = map.get(tortoise);
--- src/main/java/com/personia/hr/algorithm/GraphCycleDetector.java
+++ src/main/java/com/personia/hr/algorithm/GraphCycleDetector.java
@@ -12,6 +12,15 @@ public class GraphCycleDetector {
 
     private final FloydsTraverseAlgorithm floydsTraverseAlgorithm;
 
+    /**
+     * Partial implementation of Floyd's tortoise and hare algorithm, only detection of cycle
+     * (see See <a href="https://en.wikipedia.org/wiki/Cycle_detection">http://https://en.wikipedia.org/wiki/Cycle_detection</a>)
+     * This part of the algorithm is O(mu) being mu the position in the map of the start of the first cycle.
+     * The whole algorithm, including floyds travers, will be under O(lambda+mu)
+     *
+     * @param map the mapping function from employee to supervisor
+     * @throws LoopInEmployeeHierarchyException if a loop is detected
+     */
     public void execute(final Map<String,String> map) throws LoopInEmployeeHierarchyException {
         if (!map.isEmpty()) {
             for (String element: map.keySet()) {
